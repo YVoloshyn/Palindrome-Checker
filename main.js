c41264db0a70aaef45039bcf95730ce9f0d681c5
Yaroslav Voloshyn <jarik.voloshyn@gmail.com>
2018-07-04 16:32:47 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+function palindrome(str) {
+  str = str.replace(/[\W_]/g, "");
+    
+  var reverseStr =str.toLowerCase().split("").reverse().join("");
+  
+  if(str.toLowerCase() === reverseStr) {
+      return true;
+  } else {
+      return false;
+  }
+}
+
+palindrome("eye");
